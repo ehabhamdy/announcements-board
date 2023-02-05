f68da6e86bfa9cf355eb184f67163c15ae876cad
Hussein <I575965@d44fy6lg43.fritz.box>
2023-02-05 14:28:37 +0100

    Implement form validation

--- react-frontend/src/announcements-board/components/create-announcement/CreateAnnouncement.jsx
+++ react-frontend/src/announcements-board/components/create-announcement/CreateAnnouncement.jsx
@@ -3,26 +3,38 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 function CreateAnnouncement({ onCreateAnnouncement }) {
-  const [formErrors, setformErrors] = useState(false);
+  const [isValid, setIsValid] = useState(true);
   const [enteredAnnouncement, setEnteredAnnouncement] = useState("");
 
   const announcementChangeHandler = (event) => {
-    // setformErrors(true);
     setEnteredAnnouncement(event.target.value);
+    console.log("change");
+    if (event.target.value.trim().length === 0) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    onCreateAnnouncement(enteredAnnouncement);
+    if (enteredAnnouncement.trim().length === 0) {
+      setIsValid(false);
+    }
+    // onCreateAnnouncement(enteredAnnouncement);
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <p htmlFor="announcementContent" className="form-label fs-6">
+      <label
+        htmlFor="announcementContent"
+        className={`form-label fs-6 ${isValid ? "valid" : "noValid"}`}
+        style={{ color: !isValid ? "red" : "black" }}
+      >
         Announcement
-      </p>
+      </label>
       <textarea
-        required
+        // required
         name="contentInput"
         id="announcementContent"
         rows="3"
@@ -30,9 +42,10 @@ function CreateAnnouncement({ onCreateAnnouncement }) {
         className="form-control"
         onChange={announcementChangeHandler}
         value={enteredAnnouncement}
+        style={{ borderColor: !isValid ? "red" : "black" }}
       ></textarea>
 
-      {formErrors ? (
+      {!isValid ? (
         <p className="bd-highlight fs-7 text-danger">
           Please enter the announcement content before submitting the form
         </p>
